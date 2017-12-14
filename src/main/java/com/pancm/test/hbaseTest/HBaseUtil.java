32c1d234de542cafd6c2be1fd24a8b2fe0216eb1
xuwujing <1060589146@qq.com>
2017-12-14 10:45:07 +0800

    1.hbase方法更改 2.删除多余的类

--- src/main/java/com/pancm/test/hbaseTest/HBaseUtil.java
+++ src/main/java/com/pancm/test/hbaseTest/HBaseUtil.java
@@ -1,7 +1,10 @@
 package com.pancm.test.hbaseTest;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
@@ -13,6 +16,7 @@ import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
@@ -21,218 +25,352 @@ import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.util.Bytes;
 
+import com.alibaba.fastjson.JSONObject;
+
 /**
  * 
-* Title: HBaseUtil
-* Description: HBase工具类
-* Version:1.0.0  
-* @author pancm
-* @date 2017年12月6日
+ * Title: HBaseUtil
+ * Description: HBase工具类 
+ * Version:1.0.0
+ * @author pancm
+ * @date 2017年12月6日
  */
 public class HBaseUtil {
 	/** hadoop 连接 */
-	   private static Configuration conf=null;
-	   /**hbase 连接 */
-	   private static Connection con=null;
-	   /** 会话 */
-	   private static Admin admin = null;
-	   
-	   // 初始化连接
-	   static {
-		   // 获得配制文件对象
-	       conf = HBaseConfiguration.create(); 
-	       // 设置配置参数
-			conf.set("hbase.zookeeper.quorum", "192.169.0.23");
-			conf.set("hbase.zookeeper.property.clientPort", "2181");  
-			//如果hbase是集群，这个必须加上 
-			//ip 和端口是在配置文件配置的
-			conf.set("hbase.master", "192.169.0.23:9001"); 
-	       try {
-	           con = ConnectionFactory.createConnection(conf);// 获得连接对象
-	       } catch (IOException e) {
-	           e.printStackTrace();
-	           System.exit(1);
-	       }
-	   }
-	   
-		/**
-		 * 获取连接
-		 * @return
-		 */
-		public synchronized static Connection getConnection(){
-			if (null == con||con.isClosed()) {
-				try {
-					 con = ConnectionFactory.createConnection(conf);// 获得连接对象
-				} catch (Exception e) {
-					e.printStackTrace();
-					System.out.println("获取数据库连接失败！");
-				}
-			}
-			
-			return con;
-			
-		}
-	   
-		/**
-		 * 创建表
-		 * @param tableName 表名
-		 * @param columnFamily 列族
-		 */
-		public static void creatTable(String tableName,String [] columnFamily){
-			// 创建表名对象
-			TableName tn = TableName.valueOf(tableName);
-			// a.判断数据库是否存在
-			try {
-				//获取会话
-				admin = con.getAdmin();
-				if (admin.tableExists(tn)) {
-					System.out.println(tableName+" 表存在，删除表....");
-					// 先使表设置为不可编辑
-					admin.disableTable(tn);
-					// 删除表
-					admin.deleteTable(tn);
-					System.out.println("表删除成功.....");
-				}
-				// 创建表结构对象
-				HTableDescriptor htd = new HTableDescriptor(tn);
-				for(String str:columnFamily){
-					// 创建列族结构对象
-					HColumnDescriptor hcd = new HColumnDescriptor(str);
-					htd.addFamily(hcd);
-				}
-				// 创建表
-				admin.createTable(htd);
-				System.out.println(tableName+" 表创建成功！");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}finally{
-				close();
-			}
-		}
-		
-		/**
-		 * 数据插入或更新
-		 * @param tableName 	表名
-		 * @param rowKey   		行健  (主键)
-		 * @param family  		列族
-		 * @param qualifier   	列
-		 * @param value    		存入的值
-		 * @return
-		 */
-		public static void insert(String tableName, String rowKey,
-	            String family, String qualifier, String value){
-			 Table t=null;
-			try {
-		             t = con.getTable(TableName.valueOf(tableName));
-		            Put put = new Put(Bytes.toBytes(rowKey));
-		            put.addColumn(Bytes.toBytes(family), Bytes.toBytes(qualifier),
-		 	                    Bytes.toBytes(value));
-		           t.put(put);
-		          System.out.println(tableName+" 更新成功!");
-		        } catch (IOException e) {
-		        	System.out.println(tableName+" 更新失败!");
-		            e.printStackTrace();
-		        } finally {
-		            close();
-		       }
-		}
-		
-		/**
-		 * 查询该表中的所有数据
-		 * @param tableName 表名
-		 */
-		public static void select(String tableName){
-			Table t =null;
-			 try {
-				 t = con.getTable(TableName.valueOf(tableName));
-				// 读取操作	
-				Scan scan = new Scan();
-				// 得到扫描的结果集
-				ResultScanner rs = t.getScanner(scan);
-				for (Result result : rs) {
-					// 得到单元格集合
-					List<Cell> cs = result.listCells();
-					for (Cell cell : cs) {
-						// 取行健
-						String rowKey = Bytes.toString(CellUtil.cloneRow(cell));
-						// 取到时间戳
-						long timestamp = cell.getTimestamp();
-						// 取到族列
-						String family = Bytes.toString(CellUtil.cloneFamily(cell));
-						// 取到修饰名
-						String qualifier = Bytes.toString(CellUtil
-								.cloneQualifier(cell));
-						// 取到值
-						String value = Bytes.toString(CellUtil.cloneValue(cell));
+	private static Configuration conf = null;
+	/** hbase 连接 */
+	private static Connection con = null;
+	/** 会话 */
+	private static Admin admin = null;
 
-						System.out.println("通过表名查询所有:" + " ===> rowKey : " + rowKey
-								+ ",  timestamp : " + timestamp + ", family : "
-								+ family + ", qualifier : " + qualifier
-								+ ", value : " + value);
-					}
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+	private static String ip ="master";
+    private static String port ="2181";
+    private static String port1 ="9001";
+	   
+   // 初始化连接
+   static {
+	   // 获得配制文件对象
+       conf = HBaseConfiguration.create(); 
+       // 设置配置参数
+		conf.set("hbase.zookeeper.quorum", ip);
+		conf.set("hbase.zookeeper.property.clientPort", port);  
+		//如果hbase是集群，这个必须加上 
+		//这个ip和端口是在hadoop/mapred-site.xml配置文件配置的
+		conf.set("hbase.master", ip+":"+port1); 
+   }
 		
-		/**
-		 * 根据条件明细查询
-		 * @param tableName 	表名
-		 * @param rowKey   		行健  (主键)
-		 * @param family  		列族
-		 * @param qualifier   	列
-		 */
-		public static void select(String tableName,String rowKey,String family, String qualifier){
-			System.out.println();
-			Table t =null;
-			 try {
-				 t = con.getTable(TableName.valueOf(tableName));
-				// 通过HBase中的 get来进行查询
-				Get get = new Get(Bytes.toBytes(rowKey));
-				if(null!=family&&family.length()>0){
-					if(null!=qualifier&&qualifier.length()>0){
-						get.addColumn(Bytes.toBytes(family), Bytes.toBytes(qualifier));
-					}else{
-						get.addFamily(Bytes.toBytes(family));
-					}
-				}
-				Result r = t.get(get);
-				List<Cell> cs = r.listCells();
-				if(null==cs||cs.size()==0){
-					return;
-				}
-					for (Cell cell : cs) {
-						String rowKey1 = Bytes.toString(CellUtil.cloneRow(cell)); // 取行键
-						long timestamp = cell.getTimestamp(); // 取到时间戳
-						String family1 = Bytes.toString(CellUtil.cloneFamily(cell)); // 取到族列
-						String qualifier1 = Bytes.toString(CellUtil.cloneQualifier(cell)); // 取到列
-						String value = Bytes.toString(CellUtil.cloneValue(cell)); // 取到值
-						System.out.println("通过条件查询:"+" ===> rowKey : " + rowKey1
-								+ ",  timestamp : " + timestamp + ", family : "
-								+ family1 + ", qualifier : " + qualifier1 + ", value : "
-								+ value);
-				}
-					
-			} catch (IOException e) {
-				e.printStackTrace();
+
+	/**
+	 * 获取连接
+	 * 
+	 * @return
+	 */
+	public synchronized static Connection getConnection() {
+		try {
+			if (null == con || con.isClosed()) {
+				// 获得连接对象
+				con = ConnectionFactory.createConnection(conf);
 			}
+		} catch (IOException e) {
+			System.out.println("获取连接失败!");
+			e.printStackTrace();
 		}
-		
-		/**
-		 * 连接关闭
-		 */
-	    public static void close() {
-	    	try {
-	    		if(admin!=null){
-				   admin.close();
-	    		}
-//	    		 if (con != null) {
-//	    			 con.close();
-//	    		 }
-	    	} catch (IOException e) {
-				e.printStackTrace();
+
+		return con;
+	}
+
+	/**
+	 * 连接关闭
+	 */
+	public static void close() {
+		try {
+			if (admin != null) {
+				admin.close();
 			}
-	    }
-	    
+			if (con != null) {
+				con.close();
+			}
+		} catch (IOException e) {
+			System.out.println("连接关闭失败！");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 创建表
+	 * 
+	 * @param tableName
+	 *            表名
+	 * @param columnFamily
+	 *            列族
+	 */
+	public static void creatTable(String tableName, String[] columnFamily) {
+		if(null==tableName||tableName.length()==0){
+			return;
+		}
+		if(null==columnFamily||columnFamily.length==0){
+			return;
+		}
+		// 创建表名对象
+		TableName tn = TableName.valueOf(tableName);
+		// a.判断数据库是否存在
+		try {
+			// 获取会话
+			admin = getConnection().getAdmin();
+			if (admin.tableExists(tn)) {
+				System.out.println(tableName + " 表存在，删除表....");
+				// 先使表设置为不可编辑
+				admin.disableTable(tn);
+				// 删除表
+				admin.deleteTable(tn);
+				System.out.println("表删除成功.....");
+			}
+			// 创建表结构对象
+			HTableDescriptor htd = new HTableDescriptor(tn);
+			for (String str : columnFamily) {
+				// 创建列族结构对象
+				HColumnDescriptor hcd = new HColumnDescriptor(str);
+				htd.addFamily(hcd);
+			}
+			// 创建表
+			admin.createTable(htd);
+			System.out.println(tableName + " 表创建成功！");
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+	}
+
+	/**
+	 * 数据单条插入或更新
+	 * 
+	 * @param tableName
+	 *            表名
+	 * @param rowKey
+	 *            行健 (主键)
+	 * @param family
+	 *            列族
+	 * @param qualifier
+	 *            列
+	 * @param value
+	 *            存入的值
+	 * @return
+	 */
+	public static void insert(String tableName, String rowKey, String family,
+			String qualifier, String value) {
+		Table t = null;
+		try {
+			t = getConnection().getTable(TableName.valueOf(tableName));
+			Put put = new Put(Bytes.toBytes(rowKey));
+			put.addColumn(Bytes.toBytes(family), Bytes.toBytes(qualifier),
+					Bytes.toBytes(value));
+			t.put(put);
+			System.out.println(tableName + " 更新成功!");
+		} catch (IOException e) {
+			System.out.println(tableName + " 更新失败!");
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+	}
+
+	/**
+	 * 数据批量插入或更新
+	 * 
+	 * @param tableName
+	 *            表名
+	 * @param list
+	 *            hbase的数据 
+	 * @return
+	 */
+	public static void insertBatch(String tableName, List<?> list) {
+		if (null == tableName ||tableName.length()==0) {
+			return;
+		}
+		if( null == list || list.size() == 0){
+			return;
+		}
+		Table t = null;
+		Put put = null;
+		JSONObject json = null;
+		List<Put> puts = new ArrayList<Put>();
+		try {
+			t = getConnection().getTable(TableName.valueOf(tableName));
+			for (int i = 0, j = list.size(); i < j; i++) {
+				json = (JSONObject) list.get(i);
+				put = new Put(Bytes.toBytes(json.getString("rowKey")));
+				put.addColumn(Bytes.toBytes(json.getString("family")),
+						Bytes.toBytes(json.getString("qualifier")),
+						Bytes.toBytes(json.getString("value")));
+				puts.add(put);
+			}
+			t.put(puts);
+			System.out.println(tableName + " 更新成功!");
+		} catch (IOException e) {
+			System.out.println(tableName + " 更新失败!");
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+	}
+
+	
+	/**
+	 * 数据删除 
+	 * @param tableName 表名
+	 * @param rowKey	行健
+	 * @param family	列族
+	 * @param qualifier 列
+	 * @return
+	 */
+    public static void delete(String tableName, String rowKey, String family,
+            String qualifier) {
+    	if (null == tableName ||tableName.length()==0) {
+			return;
+		}
+		if( null == rowKey || rowKey.length() == 0){
+			return;
+		}
+    	Table t = null;
+        try {
+            t = getConnection().getTable(TableName.valueOf(tableName));
+            Delete del = new Delete(Bytes.toBytes(rowKey));
+            // 如果列族不为空
+ 			if (null != family && family.length() > 0) {
+ 				// 如果列不为空
+ 				if (null != qualifier && qualifier.length() > 0) {
+ 					del.addColumn(Bytes.toBytes(family),
+ 							Bytes.toBytes(qualifier));
+ 				} else {
+ 					del.addFamily(Bytes.toBytes(family));
+ 				}
+ 			}      
+            t.delete(del);    
+        } catch (IOException e) {
+        	System.out.println("删除失败!");
+            e.printStackTrace();
+        } finally {
+          close();
+        }
+    }
+	
+	/**
+	 * 查询该表中的所有数据
+	 * 
+	 * @param tableName
+	 *            表名
+	 */
+	public static void select(String tableName) {
+		if(null==tableName||tableName.length()==0){
+			return;
+		}
+		Table t = null;
+		List<Map<String,Object>> list=new ArrayList<Map<String,Object>>();
+		try {
+			t = getConnection().getTable(TableName.valueOf(tableName));
+			// 读取操作
+			Scan scan = new Scan();
+			// 得到扫描的结果集
+			ResultScanner rs = t.getScanner(scan);
+			if (null == rs ) {
+				return;
+			}
+			for (Result result : rs) {
+				// 得到单元格集合
+				List<Cell> cs = result.listCells();
+				if (null == cs || cs.size() == 0) {
+					continue;
+				}
+				for (Cell cell : cs) {
+					Map<String,Object> map=new HashMap<String, Object>();
+					map.put("rowKey", Bytes.toString(CellUtil.cloneRow(cell)));// 取行健
+					map.put("timestamp", cell.getTimestamp());// 取到时间戳
+					map.put("family", Bytes.toString(CellUtil.cloneFamily(cell)));// 取到列族
+					map.put("qualifier", Bytes.toString(CellUtil.cloneQualifier(cell)));// 取到列
+					map.put("value", Bytes.toString(CellUtil.cloneValue(cell)));// 取到值
+					list.add(map);
+				}
+			}
+			System.out.println("查询的数据:"+list);
+		} catch (IOException e) {
+			System.out.println("查询失败!");
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+	}
+
+	/**
+	 * 根据表名和行健查询
+	 * @param tableName
+	 * @param rowKey
+	 */
+	public static void select(String tableName, String rowKey) {
+		select(tableName,rowKey,"","");
+	}
+	
+	/**
+	 * 根据表名、行健和列族查询
+	 * @param tableName
+	 * @param rowKey
+	 * @param family
+	 */
+	public static void select(String tableName, String rowKey, String family) {
+		select(tableName,rowKey,family,"");
+	}
+	
+	/**
+	 * 根据条件明细查询
+	 * 
+	 * @param tableName
+	 *            表名
+	 * @param rowKey
+	 *            行健 (主键)
+	 * @param family
+	 *            列族
+	 * @param qualifier
+	 *            列
+	 */
+	public static void select(String tableName, String rowKey, String family,
+			String qualifier) {
+		Table t = null;
+		List<Map<String,Object>> list=new ArrayList<Map<String,Object>>();
+		try {
+			t = getConnection().getTable(TableName.valueOf(tableName));
+			// 通过HBase中的 get来进行查询
+			Get get = new Get(Bytes.toBytes(rowKey));
+			// 如果列族不为空
+			if (null != family && family.length() > 0) {
+				// 如果列不为空
+				if (null != qualifier && qualifier.length() > 0) {
+					get.addColumn(Bytes.toBytes(family),
+							Bytes.toBytes(qualifier));
+				} else {
+					get.addFamily(Bytes.toBytes(family));
+				}
+			}
+			Result r = t.get(get);
+			List<Cell> cs = r.listCells();
+			if (null == cs || cs.size() == 0) {
+				return;
+			}
+			for (Cell cell : cs) {
+				Map<String,Object> map=new HashMap<String, Object>();
+				map.put("rowKey", Bytes.toString(CellUtil.cloneRow(cell)));// 取行健
+				map.put("timestamp", cell.getTimestamp());// 取到时间戳
+				map.put("family", Bytes.toString(CellUtil.cloneFamily(cell)));// 取到列族
+				map.put("qualifier", Bytes.toString(CellUtil.cloneQualifier(cell)));// 取到列
+				map.put("value", Bytes.toString(CellUtil.cloneValue(cell)));// 取到值
+				list.add(map);
+			}
+			System.out.println("查询的数据:"+list);
+		} catch (IOException e) {
+			System.out.println("查询失败!");
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+	}
+
 }
--- src/main/java/com/pancm/test/hbaseTest/hbaseTest.java
+++ src/main/java/com/pancm/test/hbaseTest/hbaseTest.java
@@ -32,227 +32,8 @@ import com.alibaba.fastjson.JSONObject;
 * @date 2017年11月23日
  */
 public class hbaseTest {
-	/** hadoop 连接 */
-   private static Configuration conf=null;
-   /**hbase 连接 */
-   private static Connection con=null;
-   /** 会话 */
-   private static Admin admin = null;
-   
-   // 初始化连接
-   static {
-	   // 获得配制文件对象
-       conf = HBaseConfiguration.create(); 
-       // 设置配置参数
-		conf.set("hbase.zookeeper.quorum", "192.169.0.23");
-		conf.set("hbase.zookeeper.property.clientPort", "2181");  
-		//如果hbase是集群，这个必须加上 
-		//ip 和端口是在配置文件配置的
-		conf.set("hbase.master", "192.169.0.23:9001"); 
-       try {
-           con = ConnectionFactory.createConnection(conf);// 获得连接对象
-       } catch (IOException e) {
-           e.printStackTrace();
-           System.exit(1);
-       }
-   }
 	
 	
-	/**
-	 * 创建表
-	 * @param tableName 表名
-	 * @param columnFamily 列族
-	 */
-	public static void creatTable(String tableName,String [] columnFamily){
-		// 创建表名对象
-		TableName tn = TableName.valueOf(tableName);
-		// a.判断数据库是否存在
-		try {
-			//获取会话
-			admin = con.getAdmin();
-			if (admin.tableExists(tn)) {
-				System.out.println(tableName+" 表存在，删除表....");
-				// 先使表设置为不可编辑
-				admin.disableTable(tn);
-				// 删除表
-				admin.deleteTable(tn);
-				System.out.println("表删除成功.....");
-			}
-			// 创建表结构对象
-			HTableDescriptor htd = new HTableDescriptor(tn);
-			for(String str:columnFamily){
-				// 创建列族结构对象
-				HColumnDescriptor hcd = new HColumnDescriptor(str);
-				htd.addFamily(hcd);
-			}
-			// 创建表
-			admin.createTable(htd);
-			System.out.println(tableName+" 表创建成功！");
-		} catch (IOException e) {
-			e.printStackTrace();
-		}finally{
-			close();
-		}
-	}
-	
-	/**
-	 * 数据单条插入或更新
-	 * @param tableName 	表名
-	 * @param rowKey   		行健  (主键)
-	 * @param family  		列族
-	 * @param qualifier   	列
-	 * @param value    		存入的值
-	 * @return
-	 */
-	public static void insert(String tableName, String rowKey,
-            String family, String qualifier, String value){
-		 Table t=null;
-		try {
-	             t = con.getTable(TableName.valueOf(tableName));
-	            Put put = new Put(Bytes.toBytes(rowKey));
-	            put.addColumn(Bytes.toBytes(family), Bytes.toBytes(qualifier),
-	 	                    Bytes.toBytes(value));
-	           t.put(put);
-//	          System.out.println(tableName+" 更新成功!");
-	        } catch (IOException e) {
-	        	System.out.println(tableName+" 更新失败!");
-	            e.printStackTrace();
-	        } finally {
-	            close();
-	       }
-	}
-	
-	
-	/**
-	 * 数据批量插入或更新
-	 * @param tableName 	表名
-	 * @param list   		hbase的数据
-	 * @return
-	 */
-	public static void insertBatch(String tableName,List<?> list){
-		 if(null==tableName||null==list||list.size()==0){
-			 return;
-		 }
-		 Table t=null;	
-		 Put put=null;
-		 JSONObject json=null;
-		 List<Put> puts=new ArrayList<Put>();
-		try {
-	            t = con.getTable(TableName.valueOf(tableName));
-	            for(int i=0,j=list.size();i<j;i++){
-	            	json=(JSONObject) list.get(i);
-	                put = new Put(Bytes.toBytes(json.getString("rowKey")));
-	  	            put.addColumn(Bytes.toBytes(json.getString("family")), Bytes.toBytes(json.getString("qualifier")),
-	  	 	                    Bytes.toBytes(json.getString("value")));
-	  	           puts.add(put);
-	            }
-	           t.put(puts);
-	          System.out.println(tableName+" 更新成功!");
-	        } catch (IOException e) {
-	        	System.out.println(tableName+" 更新失败!");
-	            e.printStackTrace();
-	        } finally {
-	            close();
-	       }
-	}
-	
-	/**
-	 * 查询该表中的所有数据
-	 * @param tableName 表名
-	 */
-	public static void select(String tableName){
-		Table t =null;
-		 try {
-			 t = con.getTable(TableName.valueOf(tableName));
-			// 读取操作	
-			Scan scan = new Scan();
-			// 得到扫描的结果集
-			ResultScanner rs = t.getScanner(scan);
-			for (Result result : rs) {
-				// 得到单元格集合
-				List<Cell> cs = result.listCells();
-				for (Cell cell : cs) {
-					// 取行健
-					String rowKey = Bytes.toString(CellUtil.cloneRow(cell));
-					// 取到时间戳
-					long timestamp = cell.getTimestamp();
-					// 取到族列
-					String family = Bytes.toString(CellUtil.cloneFamily(cell));
-					// 取到修饰名
-					String qualifier = Bytes.toString(CellUtil
-							.cloneQualifier(cell));
-					// 取到值
-					String value = Bytes.toString(CellUtil.cloneValue(cell));
-
-					System.out.println("通过表名查询所有:" + " ===> rowKey : " + rowKey
-							+ ",  timestamp : " + timestamp + ", family : "
-							+ family + ", qualifier : " + qualifier
-							+ ", value : " + value);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * 根据条件明细查询
-	 * @param tableName 	表名
-	 * @param rowKey   		行健  (主键)
-	 * @param family  		列族
-	 * @param qualifier   	列
-	 */
-	public static void select(String tableName,String rowKey,String family, String qualifier){
-		System.out.println();
-		Table t =null;
-		 try {
-			 t = con.getTable(TableName.valueOf(tableName));
-			// 通过HBase中的 get来进行查询
-			Get get = new Get(Bytes.toBytes(rowKey));
-			if(null!=family&&family.length()>0){
-				if(null!=qualifier&&qualifier.length()>0){
-					get.addColumn(Bytes.toBytes(family), Bytes.toBytes(qualifier));
-				}else{
-					get.addFamily(Bytes.toBytes(family));
-				}
-			}
-			Result r = t.get(get);
-			List<Cell> cs = r.listCells();
-			if(null==cs||cs.size()==0){
-				return;
-			}
-				for (Cell cell : cs) {
-					String rowKey1 = Bytes.toString(CellUtil.cloneRow(cell)); // 取行键
-					long timestamp = cell.getTimestamp(); // 取到时间戳
-					String family1 = Bytes.toString(CellUtil.cloneFamily(cell)); // 取到族列
-					String qualifier1 = Bytes.toString(CellUtil.cloneQualifier(cell)); // 取到列
-					String value = Bytes.toString(CellUtil.cloneValue(cell)); // 取到值
-					System.out.println("通过条件查询:"+" ===> rowKey : " + rowKey1
-							+ ",  timestamp : " + timestamp + ", family : "
-							+ family1 + ", qualifier : " + qualifier1 + ", value : "
-							+ value);
-			}
-				
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	/**
-	 * 连接关闭
-	 */
-    public static void close() {
-    	try {
-    		if(admin!=null){
-			   admin.close();
-    		}
-//    		 if (con != null) {
-//    			 con.close();
-//    		 }
-    	} catch (IOException e) {
-			e.printStackTrace();
-		}
-    }
     
    /**
     * 批量测试方法
@@ -279,7 +60,7 @@ public class hbaseTest {
 		
 			list.add(json);
 		}
-		insertBatch(tableName,list);
+		HBaseUtil.insertBatch(tableName,list);
    }
     
     
@@ -330,7 +111,7 @@ public class hbaseTest {
 //		String tableName="t_student";
 		//将关系型数据的表对应成列族 
 		String []columnFamily={"t_student","t_class","t_grade","t_subject"};
- 		creatTable(tableName,columnFamily);
+		HBaseUtil.creatTable(tableName,columnFamily);
         //设置列键(主键，可以一对多)
  		String rowKey1="1",rowKey2="2",rowKey3="3",rowKey4="4";
 		//将关系型数据库表字段对应成列
@@ -341,24 +122,24 @@ public class hbaseTest {
 		
 // 		insert(tableName,"101","t_hive_hbase_test","uid","101");
  		
-		insert(tableName,rowKey1,columnFamily[0],id,id1);
-		insert(tableName,rowKey1,columnFamily[0],name,name1);
-		insert(tableName,rowKey1,columnFamily[0],age,age1);
-		insert(tableName,rowKey1,columnFamily[1],id,id1);
-		insert(tableName,rowKey1,columnFamily[1],name,name1);
-		insert(tableName,rowKey1,columnFamily[1],age,age1);
+ 		HBaseUtil.insert(tableName,rowKey1,columnFamily[0],id,id1);
+ 		HBaseUtil.insert(tableName,rowKey1,columnFamily[0],name,name1);
+ 		HBaseUtil.insert(tableName,rowKey1,columnFamily[0],age,age1);
+ 		HBaseUtil.insert(tableName,rowKey1,columnFamily[1],id,id1);
+ 		HBaseUtil.insert(tableName,rowKey1,columnFamily[1],name,name1);
+ 		HBaseUtil.insert(tableName,rowKey1,columnFamily[1],age,age1);
 		
-		insert(tableName,rowKey2,columnFamily[0],id,id2);
-		insert(tableName,rowKey2,columnFamily[0],name,name2);
-		insert(tableName,rowKey2,columnFamily[0],age,age2);
-		insert(tableName,rowKey3,columnFamily[3],id,id1);
-		insert(tableName,rowKey3,columnFamily[3],name,name1);
-		insert(tableName,rowKey3,columnFamily[3],age,age1);
+ 		HBaseUtil.insert(tableName,rowKey2,columnFamily[0],id,id2);
+ 		HBaseUtil.insert(tableName,rowKey2,columnFamily[0],name,name2);
+ 		HBaseUtil.insert(tableName,rowKey2,columnFamily[0],age,age2);
+ 		HBaseUtil.insert(tableName,rowKey3,columnFamily[3],id,id1);
+ 		HBaseUtil.insert(tableName,rowKey3,columnFamily[3],name,name1);
+ 		HBaseUtil.insert(tableName,rowKey3,columnFamily[3],age,age1);
  		long starTime=System.currentTimeMillis(); 
-		select(tableName);
-		select(tableName,"1001","","");
-		select(tableName,rowKey1,columnFamily[0],"");
-		select(tableName,rowKey1,columnFamily[0],id);
+ 		HBaseUtil.select(tableName);
+ 		HBaseUtil.select(tableName,"1001","","");
+ 		HBaseUtil.select(tableName,rowKey1,columnFamily[0],"");
+ 		HBaseUtil.select(tableName,rowKey1,columnFamily[0],id);
 		long endTime=System.currentTimeMillis(); 
     	System.out.println("查询共使用时间为:"+(endTime-starTime)+"毫秒");
 	}
