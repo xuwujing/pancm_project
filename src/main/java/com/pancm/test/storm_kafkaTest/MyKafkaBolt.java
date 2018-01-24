1ed83bda7b988647cc43fe63aa14237583924ffc
xuwujing <1060589146@qq.com>
2018-01-24 16:29:46 +0800

    1.新增logback日志文件 2.完成logback日志文件的测试

--- src/main/java/com/pancm/test/storm_kafkaTest/MyKafkaBolt.java
+++ src/main/java/com/pancm/test/storm_kafkaTest/MyKafkaBolt.java
@@ -29,7 +29,13 @@ public class MyKafkaBolt implements IBasicBolt {
         // TODO Auto-generated method stub
 
     }
-
+    /**
+     * 
+    * 每次调用处理一个输入的tuple，当然，也可以把tuple暂存起来批量处理。
+    * 但是！！！千万注意，所有的tuple都必须在一定时间内应答，可以是ack或者fail。否则，spout就会重发tuple。
+    * 两个bolt不同的地方在于,IBasicBolt自动帮你ack，而IRichBolt需要你自己来做。
+    */
+     
     @Override
     public void execute(Tuple input, BasicOutputCollector collector) {
     	//消费的kafka
