880638a3ae987feed54034310ce69c142890f7dc
xuwujing <1060589146@qq.com>
2018-08-15 17:10:34 +0800

    建造者模式提交

--- src/main/java/com/pancm/test/design/BuilderTest/BuilderTest.java
+++ src/main/java/com/pancm/test/design/BuilderTest/BuilderTest.java
@@ -4,18 +4,20 @@ package com.pancm.test.design.BuilderTest;
 * @Title: BuilderTest
 * @Description: 
 * 建造者模式
+* 将一个复杂的构建与其表示相分离，使得同样的构建过程可以创建不同的表示。
 * @Version:1.0.0  
 * @author pancm
 * @date 2018年8月7日
 */
 public class BuilderTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		FoodStore foodStore=new FoodStore();
+		Meal meal=foodStore.createBreakfast(new Breakfast());
+		Meal meal2=foodStore.createBreakfast(new Lunch());
+		System.out.println("小明早上吃的是:"+meal.getFood()+",喝的饮料是:"+meal.getDrinks());
+		System.out.println("小明中午吃的是:"+meal2.getFood()+",喝的饮料是:"+meal2.getDrinks());
+		
 	}
 
 }
@@ -24,23 +26,29 @@ public class BuilderTest {
  * 
 * @Title: Breakfast
 * @Description: 
-* 定义一份早餐
+* 定义一份餐点
 * 分为吃的和喝的
 * @Version:1.0.0  
 * @author pancm
 * @date 2018年8月7日
  */
-class Breakfast{
+class Meal{
 	private String food;
 	private String drinks;
+	
+	public String getFood() {
+		return food;
+	}
 	public void setFood(String food) {
 		this.food = food;
+	}
+	
+	public String getDrinks() {
+		return drinks;
 	}
 	public void setDrinks(String drinks) {
 		this.drinks = drinks;
 	}
-	
-		
 }
 
 /**
@@ -48,59 +56,93 @@ class Breakfast{
 * @Title: IBuilderFood
 * @Description: 
 * 定义一个食物接口
-*     有豆浆、牛奶、煎饼和三明治
 * @Version:1.0.0  
 * @author pancm
 * @date 2018年8月7日
  */
 interface IBuilderFood{
-	void buildSandwich();
-	void buildPancake();
-	void buildMilk();
-	void buildSoybeanMilk();
-	Breakfast createBreakfast();
+	void buildFood();
+	void buildDrinks();
+	Meal createMeal();
 }
 
 /**
  * 
-* @Title: ChinaBreakfast
-* @Description:定义一个早餐店 
+* @Title: Breakfast
+* @Description:定义一份早餐
 * @Version:1.0.0  
 * @author pancm
 * @date 2018年8月7日
  */
-class BreakfastBars implements IBuilderFood{
-	Breakfast breakfast;
-	
-	public BreakfastBars(){
-		breakfast=new Breakfast();
-	}
-	
-	@Override
-	public void buildSandwich() {
-		breakfast.setDrinks("");
-	}
+class Breakfast implements IBuilderFood{
+	Meal meal;
 
-	@Override
-	public void buildPancake() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void buildMilk() {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void buildSoybeanMilk() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public Breakfast createBreakfast() {
-		// TODO Auto-generated method stub
-		return null;
+	public Breakfast(){
+		meal=new Meal();
 	}
 	
+	@Override
+	public void buildFood() {
+		meal.setFood("煎饼");
+	}
+
+	@Override
+	public void buildDrinks() {
+		meal.setDrinks("豆浆");	
+	}
+	
+	@Override
+	public Meal createMeal() {
+		return meal;
+	}
 }
+
+/**
+ * 
+* @Title: Lunch
+* @Description: 定义一份午餐
+* @Version:1.0.0  
+* @author pancm
+* @date 2018年8月15日
+ */
+class Lunch implements IBuilderFood{
+	Meal meal;
+
+	public Lunch(){
+		meal=new Meal();
+	}
+	
+	@Override
+	public void buildFood() {
+		meal.setFood("盒饭");
+	}
+
+	@Override
+	public void buildDrinks() {
+		meal.setDrinks("果汁");	
+	}
+	
+	@Override
+	public Meal createMeal() {
+		return meal;
+	}
+}
+
+/**
+ * 
+* @Title: FoodStore
+* @Description: 
+* 定义一个餐点
+* 导演者
+* @Version:1.0.0  
+* @author pancm
+* @date 2018年8月15日
+ */
+class FoodStore{
+	public Meal createBreakfast(IBuilderFood bf){
+		bf.buildDrinks();
+		bf.buildFood();
+		return bf.createMeal();
+	}
+}
+
