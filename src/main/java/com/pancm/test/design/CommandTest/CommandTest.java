d41838a06ba3ca0d7c2233847c5be02a0c922a78
xuwujing <1060589146@qq.com>
2018-08-31 17:55:08 +0800

    组合模式提交

--- src/main/java/com/pancm/test/design/CommandTest/CommandTest.java
+++ src/main/java/com/pancm/test/design/CommandTest/CommandTest.java
@@ -13,7 +13,6 @@ public class CommandTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
--- src/main/java/com/pancm/test/design/FlyweightTest/FlyweightTest.java
+++ src/main/java/com/pancm/test/design/FlyweightTest/FlyweightTest.java
@@ -3,17 +3,14 @@ package com.pancm.test.design.FlyweightTest;
 /**
 * @Title: FlyweightTest
 * @Description: 享元模式
+* 在有大量对象时，有可能会造成内存溢出，我们把其中共同的部分抽象出来，如果有相同的业务请求，直接返回在内存中已有的对象，避免重新创建。
 * @Version:1.0.0  
 * @author pancm
 * @date 2018年8月8日
 */
 public class FlyweightTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
