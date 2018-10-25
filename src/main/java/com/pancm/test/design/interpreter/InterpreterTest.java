4460eb9a466bb6e663a9f44579de6b12660546a4
xuwujing <1060589146@qq.com>
2018-10-25 19:28:48 +0800

    解释器模式和迭代器模式提交

--- src/main/java/com/pancm/test/design/interpreter/InterpreterTest.java
+++ src/main/java/com/pancm/test/design/interpreter/InterpreterTest.java
@@ -3,6 +3,9 @@ package com.pancm.test.design.interpreter;
 /**
 * @Title: InterpreterTest
 * @Description: 解释器模式
+* 解释器模式（Interpreter Pattern）提供了评估语言的语法或表达式的方式，它属于行为型模式。
+* 这种模式实现了一个表达式接口，该接口解释一个特定的上下文。这种模式被用在 SQL 解析、符号处理引擎等。
+* 比如正则表达式
 * @Version:1.0.0  
 * @author pancm
 * @date 2018年8月8日
@@ -13,8 +16,90 @@ public class InterpreterTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		/*
+		 * 基本使用
+		 * 使用不同的解释器，输出的结果不同
+		 */
+		Context context = new Context();
+		context.setId(1);
+		context.setName("xuwujing");
+		AbstractExpreeion aExpreeion=new OneExpreeion();
+		AbstractExpreeion aExpreeion2=new TwoExpreeion();
+		aExpreeion.interpert(context);
+		aExpreeion2.interpert(context);
+		
+		
+		
+		
+		/*
+		 
+		 应用实例：编译器、运算表达式计算。
 
+		优点： 1、可扩展性比较好，灵活。 2、增加了新的解释表达式的方式。 3、易于实现简单文法。
+		
+		缺点： 1、可利用场景比较少。 2、对于复杂的文法比较难维护。 3、解释器模式会引起类膨胀。 4、解释器模式采用递归调用方法。
+		
+		使用场景： 1、可以将一个需要解释执行的语言中的句子表示为一个抽象语法树。 2、一些重复出现的问题可以用一种简单的语言来进行表达。 3、一个简单语法需要解释的场景。
+		 
+		 */
+		
+		
 	}
 
 }
+
+class Context{
+	private Integer id;
+	private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	/** 
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return "Context [id=" + id + ", name=" + name + "]";
+	}
+	
+}
+
+/*
+ * 定义一个抽象类
+ */
+abstract class AbstractExpreeion{
+	abstract void interpert(Context context);
+}
+
+/*
+ * 一个解释器
+ */
+class OneExpreeion extends AbstractExpreeion{
+	@Override
+	void interpert(Context context) {
+		System.out.println("one:"+context.toString());
+	}
+}
+
+/*
+ * 另一个解释器
+ */
+class TwoExpreeion extends AbstractExpreeion{
+	@Override
+	void interpert(Context context) {
+		System.out.println("two:"+context.toString());
+	}
+}
+
+
+
--- src/main/java/com/pancm/test/design/iterator/IteratorTest.java
+++ src/main/java/com/pancm/test/design/iterator/IteratorTest.java
@@ -1,20 +1,99 @@
 package com.pancm.test.design.iterator;
 
 /**
-* @Title: IteratorTest
-* @Description: 
-* @Version:1.0.0  
-* @author pancm
-* @date 2018年8月8日
-*/
+ * @Title: IteratorTest
+ * @Description: 迭代器模式 迭代器模式（Iterator Pattern）是 Java 和 .Net
+ *               编程环境中非常常用的设计模式。这种模式用于顺序访问集合对象的元素，不需要知道集合对象的底层表示。 迭代器模式属于行为型模式。
+ *               提供一种方法顺序访问一个聚合对象中各个元素, 而又无须暴露该对象的内部表示。
+ * @Version:1.0.0
+ * @author pancm
+ * @date 2018年8月8日
+ */
 public class IteratorTest {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		/*
+		 * 基本使用
+		 */
+		String names[] = { "zhangsan", "lisi", "wangwu", "xuwujing" };
+		NameRepository namesRepository = new NameRepository(names);
+		for (Iterator iter = namesRepository.getIterator(); iter.hasNext();) {
+			String name = (String) iter.next();
+			System.out.println("name : " + name);
+		}
+
+		/*
+		 * 
+		 * 优点： 1、它支持以不同的方式遍历一个聚合对象。 2、迭代器简化了聚合类。 3、在同一个聚合上可以有多个遍历。
+		 * 4、在迭代器模式中，增加新的聚合类和迭代器类都很方便，无须修改原有代码。
+		 * 
+		 * 缺点：由于迭代器模式将存储数据和遍历数据的职责分离，增加新的聚合类需要对应增加新的迭代器类，类的个数成对增加，这在一定程度上增加了系统的复杂性。
+		 * 
+		 * 使用场景： 1、访问一个聚合对象的内容而无须暴露它的内部表示。 2、需要为聚合对象提供多种遍历方式。 3、为遍历不同的聚合结构提供一个统一的接口。
+		 * 
+		 * 注意事项：迭代器模式就是分离了集合对象的遍历行为，抽象出一个迭代器类来负责，这样既可以做到不暴露集合的内部结构，又可让外部代码透明地访问集合内部的数据。
+		 * 
+		 */
 	}
 
 }
+
+/*
+ * 定义一个迭代器
+ */
+interface Iterator {
+
+	boolean hasNext();
+
+	Object next();
+}
+
+/*
+ * 定义一个容器
+ * 
+ */
+interface Container {
+	Iterator getIterator();
+}
+
+
+/*
+ * 定义一个
+ */
+class NameRepository implements Container {
+	public String names[];
+
+	public NameRepository(String names[]) {
+		this.names = names;
+	}
+	
+	
+	@Override
+	public Iterator getIterator() {
+		return new NameIterator();
+	}
+
+	private class NameIterator implements Iterator {
+
+		int index;
+
+		@Override
+		public boolean hasNext() {
+			if (index < names.length) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public Object next() {
+			if (this.hasNext()) {
+				return names[index++];
+			}
+			return null;
+		}
+	}
+}
+
+
