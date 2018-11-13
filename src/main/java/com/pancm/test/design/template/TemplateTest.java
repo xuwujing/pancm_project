e856c742e965bd9a04bc2db8a4fa8ae561ff13f4
xuwujing <1060589146@qq.com>
2018-11-13 16:53:25 +0800

    1.模板模式示例代码提交

--- src/main/java/com/pancm/test/design/template/TemplateTest.java
+++ src/main/java/com/pancm/test/design/template/TemplateTest.java
@@ -36,9 +36,15 @@ public class TemplateTest {
 		game.play();
 
 		/*
-		 * 优点： 1、封装不变部分，扩展可变部分。 2、提取公共代码，便于维护。 3、行为由父类控制，子类实现。
-		 * 缺点：每一个不同的实现都需要一个子类来实现，导致类的个数增加，使得系统更加庞大。 使用场景： 1、有多个子类共有的方法，且逻辑相同。
-		 * 2、重要的、复杂的方法，可以考虑作为模板方法。 注意事项：为防止恶意操作，一般模板方法都加上 final 关键词。
+		 * 优点： 
+		 * 1、封装不变部分，扩展可变部分。 
+		 * 2、提取公共代码，便于维护。
+		 * 3、行为由父类控制，子类实现。
+		 * 缺点：每一个不同的实现都需要一个子类来实现，导致类的个数增加，使得系统更加庞大。 
+		 * 使用场景： 
+		 * 1、有多个子类共有的方法，且逻辑相同。
+		 * 2、重要的、复杂的方法，可以考虑作为模板方法。
+		 *  注意事项：为防止恶意操作，一般模板方法都加上 final 关键词。
 		 * 
 		 */
 
@@ -55,7 +61,7 @@ abstract class  Game{
 	protected abstract void  runGame();
 	//选择人物
 	protected  void choosePerson() {};
-	//开始玩游戏游戏
+	//开始玩游戏
 	protected abstract void startPlayGame();
 	//结束游戏
 	protected abstract void endPlayGame();
@@ -87,8 +93,6 @@ class ContraGame extends Game{
 	protected void endPlayGame() {
 		System.out.println("1P被流弹打死了，游戏结束！");
 	}
-	
-	
 }
 
 //忍者神龟游戏
