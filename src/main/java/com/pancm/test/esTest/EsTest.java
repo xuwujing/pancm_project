2a31888784ed8d5897b62b8a22637917b44ebbb1
xuwujing <1060589146@qq.com>
2019-03-06 14:09:27 +0800

    增加ES相关的测试示例代码

---
+++ src/main/java/com/pancm/test/esTest/EsTest.java
@@ -0,0 +1,12 @@
+package com.pancm.test.esTest;
+
+/**
+* @Title: EsTest
+* @Description: 
+* @Version:1.0.0  
+* @author pancm
+* @date 2019年3月5日
+*/
+public class EsTest {
+
+}
---
+++ src/main/java/com/pancm/test/esTest/package-info.java
@@ -0,0 +1,8 @@
+/**
+* @Title: package-info
+* @Description: Es测试的相关代码
+* @Version:1.0.0  
+* @author pancm
+* @date 2019年3月5日
+*/
+package com.pancm.test.esTest;
