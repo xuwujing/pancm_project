05cebc701afb5721cb391f9fdd137907c09125f4
xuwujing <1060589146@qq.com>
2019-03-10 22:10:43 +0800

    注解类提交

---
+++ src/main/java/com/pancm/test/annotation/Check.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.pancm.test.annotation;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+/**
+* @Title: Check
+* @Description: 校验的注解
+* @Version:1.0.0  
+* @author pancm
+* @date 2018年5月10日
+*/
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RUNTIME)
+public @interface Check {
+
+	String[] value();
+
+}
