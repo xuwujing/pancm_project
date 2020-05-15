f65ad17baa22f039857fe4616a68d05ef44c7660
xuwujing <1060589146@qq.com>
2020-05-15 16:43:06 +0800

    增加kafka使用示例

--- src/main/java/com/pancm/test/kafkaTest/test1/KafkaProducerTest.java
+++ src/main/java/com/pancm/test/kafkaTest/test1/KafkaProducerTest.java
@@ -1,9 +1,12 @@
 package com.pancm.test.kafkaTest.test1;
 
+import com.alibaba.fastjson.JSONObject;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 
@@ -24,7 +27,8 @@ public class KafkaProducerTest implements Runnable {
 //	private String url= "192.169.2.98:2181,192.169.2.188:2181,192.169.2.156:2181";
 //	private String url= "192.169.2.30:9092,192.169.2.121:9092,192.169.2.184:9092";
 //	private String url= "192.169.0.23:9092,192.169.0.24:9092,192.169.0.25:9092";
-	private String url= "192.169.2.202:9092,192.169.2.248:9092,192.169.2.249:9092";
+//	private String url= "192.169.2.202:9092,192.169.2.248:9092,192.169.2.249:9092";
+	private String url= "192.169.2.41:9092";
 
 
 	/**
@@ -55,14 +59,31 @@ public class KafkaProducerTest implements Runnable {
 		try {
 			for(;;) {
 				String messageStr="topic:"+topic+" 你好，这是第"+messageNo+"条数据";
-				producer.send(new ProducerRecord<String, String>(topic,  messageStr));
+				List<ThreeSpgateBean> list = new ArrayList<>();
+				for (int i = 1; i < 10; i++) {
+					ThreeSpgateBean threeSpgateBean = new ThreeSpgateBean();
+					threeSpgateBean.setEs_status("2");
+					threeSpgateBean.setRaw_status("DELIVRD");
+					threeSpgateBean.setGateid(String.valueOf(i));
+					threeSpgateBean.setMsgtype("1");
+					threeSpgateBean.setSpgate("1");
+					threeSpgateBean.setSrctype("1");
+					threeSpgateBean.setPhone(String.valueOf((12345678901L+i)));
+					threeSpgateBean.setSpgate(String.valueOf(1000+i));
+					threeSpgateBean.setEs_uptm("2020-03-03 15:59:26.410");
+					threeSpgateBean.setRaw_recvtm("2020-03-03 15:59:26.410");
+					threeSpgateBean.setRaw_sendtm("2020-03-03 15:59:26.410");
+					list.add(threeSpgateBean);
+				}
+
+
+				producer.send(new ProducerRecord<String, String>(topic,  list.toString()));
 				//生产了100条就打印
 				if(messageNo%100==0){
 					System.out.println("发送的信息:" + messageStr);
-
 				}
 				//生产1000条就退出
-				if(messageNo%1000==0){
+				if(messageNo%10==0){
 					System.out.println("成功发送了"+messageNo+"条");
 					break;
 				}
@@ -82,7 +103,7 @@ public class KafkaProducerTest implements Runnable {
      * @param args the args
      */
     public static void main(String args[]) {
-		KafkaProducerTest test1 = new KafkaProducerTest("t1");
+		KafkaProducerTest test1 = new KafkaProducerTest("pu_cmd_spgate");
 		KafkaProducerTest test2 = new KafkaProducerTest("t2");
 		KafkaProducerTest test3= new KafkaProducerTest("t3");
 		Thread thread1 = new Thread(test1);
@@ -92,9 +113,9 @@ public class KafkaProducerTest implements Runnable {
 		try {
 			thread1.start();
 			Thread.sleep(1000);
-			thread2.start();
-			Thread.sleep(1000);
-			thread3.start();
+//			thread2.start();
+//			Thread.sleep(1000);
+//			thread3.start();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -102,4 +123,129 @@ public class KafkaProducerTest implements Runnable {
 	}
 	
 
+}
+
+class ThreeSpgateBean {
+
+	/** 数据来源 1:号码状态  */
+	private String source;
+
+	private String spgate;
+	/** 通道ID */
+	private String gateid;
+
+	private String es_status;
+
+	private String raw_status;
+	/** 手机号 */
+	private String phone;
+	/** 该条数据在空号ES库中最新的更新时间 */
+	private String es_uptm;
+
+	/** 该条数据原始的发送时间(RPT_RVOK.MTSENDTIME) */
+	private String raw_sendtm;
+
+	/** 该条数据原始的返回时间(RPT_RVOK.RECVTIME) */
+	private String raw_recvtm;
+
+
+	/** 原运营商类型 取值范围：0,1,2,5,21,255 */
+	private String srctype;
+
+	/** 消息类型 */
+	private String msgtype;
+
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getSpgate() {
+		return spgate;
+	}
+
+	public void setSpgate(String spgate) {
+		this.spgate = spgate;
+	}
+
+	public String getGateid() {
+		return gateid;
+	}
+
+	public void setGateid(String gateid) {
+		this.gateid = gateid;
+	}
+
+	public String getEs_status() {
+		return es_status;
+	}
+
+	public void setEs_status(String es_status) {
+		this.es_status = es_status;
+	}
+
+	public String getRaw_status() {
+		return raw_status;
+	}
+
+	public void setRaw_status(String raw_status) {
+		this.raw_status = raw_status;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEs_uptm() {
+		return es_uptm;
+	}
+
+	public void setEs_uptm(String es_uptm) {
+		this.es_uptm = es_uptm;
+	}
+
+	public String getRaw_sendtm() {
+		return raw_sendtm;
+	}
+
+	public void setRaw_sendtm(String raw_sendtm) {
+		this.raw_sendtm = raw_sendtm;
+	}
+
+	public String getRaw_recvtm() {
+		return raw_recvtm;
+	}
+
+	public void setRaw_recvtm(String raw_recvtm) {
+		this.raw_recvtm = raw_recvtm;
+	}
+
+	public String getSrctype() {
+		return srctype;
+	}
+
+	public void setSrctype(String srctype) {
+		this.srctype = srctype;
+	}
+
+	public String getMsgtype() {
+		return msgtype;
+	}
+
+	public void setMsgtype(String msgtype) {
+		this.msgtype = msgtype;
+	}
+
+	@Override
+	public String toString() {
+		return JSONObject.toJSONString(this);
+	}
 }
