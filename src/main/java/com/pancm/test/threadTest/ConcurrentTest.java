a8c16e2353a13e93adb0d5873325c0c33f6d015e
xuwujing <1060589146@qq.com>
2020-05-21 17:30:58 +0800

    增加并发使用示例

---
+++ src/main/java/com/pancm/test/threadTest/ConcurrentTest.java
@@ -0,0 +1,21 @@
+package com.pancm.test.threadTest;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author pancm
+ * @Title: pancm_project
+ * @Description:
+ * @Version:1.0.0
+ * @Since:jdk1.8
+ * @date 2020/5/21
+ */
+public class ConcurrentTest {
+    public static void main(String[] args) {
+        //安全的数组
+        CopyOnWriteArrayList copyOnWriteArrayList =new CopyOnWriteArrayList();
+
+
+
+    }
+}
