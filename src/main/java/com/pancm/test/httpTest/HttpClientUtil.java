0563979db06e011469b2389f1f8d0cdfba16675a
xuwujing <1060589146@qq.com>
2021-03-15 18:38:08 +0800

    1.增加HTTPS相关的类

--- src/main/java/com/pancm/test/httpTest/HttpClientUtil.java
+++ src/main/java/com/pancm/test/httpTest/HttpClientUtil.java
@@ -1,12 +1,9 @@
 package com.pancm.test.httpTest;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.util.Map;
-import java.util.Map.Entry;
-
+import com.alibaba.fastjson.JSONObject;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpPut;
@@ -16,9 +13,12 @@ import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
-import com.alibaba.fastjson.JSONObject;
-import com.pancm.util.MyTools;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Map;
+import java.util.Map.Entry;
 
 
 /**
@@ -47,12 +47,12 @@ public class HttpClientUtil {
 		 StringBuffer sb=new StringBuffer();
 		  //构建请求参数
 		 if(map!=null&&map.size()>0){
-	            for (Entry<String, Object> e : map.entrySet()) {  
+	            for (Entry<String, Object> e : map.entrySet()) {
 	            	sb.append(e.getKey());
-	            	sb.append("=");  
-	            	sb.append(e.getValue());  
-	            	sb.append("&");  
-	            }  
+	            	sb.append("=");
+	            	sb.append(e.getValue());
+	            	sb.append("&");
+	            }
 		  }
 	   return  get(url+"?"+sb.toString());
 	}
@@ -65,16 +65,16 @@ public class HttpClientUtil {
      * @return string
      * @throws Exception the exception
      */
-    public static String get(String url,JSONObject json) throws Exception {
+    public static String get(String url, JSONObject json) throws Exception {
 		 StringBuffer sb=new StringBuffer();
 		  //构建请求参数
 		 if(json!=null&&json.size()>0){
-	            for (Entry<String, Object> e : json.entrySet()) {  
+	            for (Entry<String, Object> e : json.entrySet()) {
 	            	sb.append(e.getKey());
-	            	sb.append("=");  
-	            	sb.append(e.getValue());  
-	            	sb.append("&");  
-	            }  
+	            	sb.append("=");
+	            	sb.append(e.getValue());
+	            	sb.append("&");
+	            }
 		  }
 	     return  get(url+"?"+sb.toString());
 	}
@@ -94,6 +94,10 @@ public class HttpClientUtil {
 		try {
 			httpClient = HttpClientBuilder.create().build();
 			method = new HttpGet(url);
+			// 增加http请求超时设置
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(10000)
+					.setConnectionRequestTimeout(30000).setSocketTimeout(60000).build();
+			method.setConfig(requestConfig);
 		if (method != null ) {
 				// 建立一个NameValuePair数组，用于存储欲传送的参数
 				method.addHeader("Content-type",
@@ -107,7 +111,7 @@ public class HttpClientUtil {
 					throw new Exception("statusCode：" + statusCode);
 				}
 				body = EntityUtils.toString(response.getEntity());
-			}	
+			}
 		} catch (IOException e) {
 			// 网络错误
 			throw e;
@@ -121,6 +125,57 @@ public class HttpClientUtil {
 		return body;
 	}
 
+	/**
+	 * post 请求
+	 *
+	 * @param postUrl    the post url
+	 * @param
+	 * @return string
+	 * @throws Exception the exception
+	 */
+	public static String post(String postUrl) throws Exception {
+		CloseableHttpClient httpClient= null;
+		HttpPost method = null;
+		String body = null;
+		try {
+			httpClient = HttpClientBuilder.create().build();
+			method = new HttpPost(postUrl);
+			// 增加http请求超时设置
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(60000)
+					.setConnectionRequestTimeout(60000).setSocketTimeout(60000).build();
+			method.setConfig(requestConfig);
+			if (method != null ) {
+				// 建立一个NameValuePair数组，用于存储欲传送的参数
+				method.addHeader("Content-type",
+						"application/json; charset=utf-8");
+				method.setHeader("Accept", "application/json");
+
+				HttpResponse response = httpClient.execute(method);
+
+				int statusCode = response.getStatusLine().getStatusCode();
+
+				// logger.debug("statusCode:" + statusCode);
+				if (statusCode != HttpStatus.SC_OK) {
+					logger.error("Method failed:" + response.getStatusLine());
+					throw new Exception("statusCode：" + statusCode);
+				}
+
+				// Read the response body
+				body = EntityUtils.toString(response.getEntity());
+			}
+		} catch (IOException e) {
+			// 网络错误
+			throw e;
+		} finally {
+			if(httpClient!=null){
+				httpClient.close();
+				httpClient= null;
+				method = null;
+			}
+			// logger.debug("调用接口状态：" + status);
+		}
+		return body;
+	}
 
     /**
      * post 请求
@@ -137,7 +192,72 @@ public class HttpClientUtil {
 		try {
 			httpClient = HttpClientBuilder.create().build();
 			method = new HttpPost(postUrl);
-		if (method != null & parameters!=null) {
+			// 增加http请求超时设置
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(60000)
+					.setConnectionRequestTimeout(60000).setSocketTimeout(60000).build();
+			method.setConfig(requestConfig);
+		if (method != null && parameters!=null) {
+			logger.info("请求接口的url地址:{},请求参数:{}",postUrl,JSONObject.toJSONString(parameters));
+			// 建立一个NameValuePair数组，用于存储欲传送的参数
+				method.addHeader("Content-type",
+						"application/json; charset=utf-8");
+				method.setHeader("Accept", "application/json");
+				method.setEntity(new StringEntity(parameters, Charset
+						.forName("UTF-8")));
+
+				HttpResponse response = httpClient.execute(method);
+
+				int statusCode = response.getStatusLine().getStatusCode();
+
+				// logger.debug("statusCode:" + statusCode);
+				if (statusCode != HttpStatus.SC_OK) {
+					logger.error("Method failed:" + response.getStatusLine());
+					logger.error("Request:"+parameters);
+					throw new Exception("statusCode：" + statusCode);
+				}
+
+				// Read the response body
+				body = EntityUtils.toString(response.getEntity());
+			}
+		} catch (IOException e) {
+			// 网络错误
+			throw e;
+		} finally {
+			if(httpClient!=null){
+				httpClient.close();
+				httpClient= null;
+				method = null;
+			}
+			// logger.debug("调用接口状态：" + status);
+		}
+		logger.info("接口返回参数:{}!",body);
+		return body;
+	}
+
+
+
+	/**
+	 * post 请求
+	 *
+	 * @param postUrl    the post url
+	 * @param parameters the parameters
+	 * @return string
+	 * @throws Exception the exception
+	 */
+	public static String httpsPost(String postUrl,String parameters) throws Exception {
+		CloseableHttpClient httpClient= null;
+		HttpPost method = null;
+		String body = null;
+		try {
+//			httpClient = HttpClientBuilder.create().build();
+			httpClient = new SSLClient();
+			method = new HttpPost(postUrl);
+			// 增加http请求超时设置
+			RequestConfig requestConfig = RequestConfig.custom().setConnectTimeout(60000)
+					.setConnectionRequestTimeout(60000).setSocketTimeout(60000).build();
+			method.setConfig(requestConfig);
+			if (method != null && parameters!=null) {
+				logger.info("请求接口的url地址:{},请求参数:{}",postUrl,JSONObject.toJSONString(parameters));
 				// 建立一个NameValuePair数组，用于存储欲传送的参数
 				method.addHeader("Content-type",
 						"application/json; charset=utf-8");
@@ -158,7 +278,7 @@ public class HttpClientUtil {
 
 				// Read the response body
 				body = EntityUtils.toString(response.getEntity());
-			}	
+			}
 		} catch (IOException e) {
 			// 网络错误
 			throw e;
@@ -170,6 +290,7 @@ public class HttpClientUtil {
 			}
 			// logger.debug("调用接口状态：" + status);
 		}
+		logger.info("接口返回参数:{}!",body);
 		return body;
 	}
 
@@ -188,7 +309,7 @@ public class HttpClientUtil {
 		try {
 			httpClient = HttpClientBuilder.create().build();
 			method = new HttpPut(postUrl);
-		if (method != null & MyTools.isNotEmpty(parameters)) {
+		if (method != null && !StringUtils.isEmpty(parameters)) {
 				// 建立一个NameValuePair数组，用于存储欲传送的参数
 				method.addHeader("Content-type",
 						"application/json; charset=utf-8");
@@ -205,7 +326,7 @@ public class HttpClientUtil {
 
 				// Read the response body
 				body = EntityUtils.toString(response.getEntity());
-			}	
+			}
 		} catch (IOException e) {
 			// 网络错误
 			throw e;
@@ -219,43 +340,5 @@ public class HttpClientUtil {
 		return body;
 	}
 
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
-    public static void main(String[] args) {
-		String url="http://localhost:8080/montnets/validphone";
-		JSONObject json=new JSONObject();
-		JSONObject json2=new JSONObject();
-		json2.put("datatype", 1);
-		json2.put("index", 1);
-		json2.put("pagesize", 10);
-		json2.put("startlastupdatetm", "2018-03-12 09:49:47.000");
-		
-		json.put("seqid", "201707052020666666");
-		json.put("timestamp", "20170705202015666");
-		json.put("token", "890ecc582bc0512df9e3548178782eda");
-		json.put("encryptsign", 1);
-		
-		try {
-			
-//			String  url1="http://localhost:8080/montnets/validphone?svccont=85a7c5523ccecbc5b7753e00246603cc76bb30486b212a43b4c8c3347a0b6972160eaad6df64d7d9e3af9529594dc48bfa39213404803aa461ddf5abc37152fa9f1e2d7c318d0d5f7dc85673dfa12cf559a044f3bd909e99a800b9057c7d81ba&encryptsign=1&seqid=201707052020666666&timestamp=2017-07-05%2020:20:15%20666&token=b48630d4ec5d11982fc85e2e1456c2e0&";
-			String getTest1=get(url,json);
-			System.out.println("GET请求:"+getTest1);
-			
-			String postTest=post(url,json.toJSONString());
-			System.out.println("POST请求:"+postTest);
-			
-			String pupTest=put(url,json.toJSONString());
-			System.out.println("PUT请求:"+pupTest);
-			
-		    
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
+
 }
