1a8db54ff6fc0ce096d76104f23d0071db9964bf
beixing <beixing@lgwangyu.com>
2022-03-02 15:02:38 +0800

    1.excel更新

---
+++ src/main/java/com/pancm/test/excel/EasyExcelWriteTest.java
@@ -0,0 +1,94 @@
+package com.pancm.test.excel;
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelWriter;
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.write.metadata.WriteSheet;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author beixing
+ * @Title: pancm_project
+ * @Description: excel写模块测试
+ * @Version:1.0.0
+ * @Since:jdk1.8
+ * @date 2022/3/2
+ */
+@Slf4j
+public class EasyExcelWriteTest {
+
+    public void excludeOrIncludeWrite() {
+        // 注意 simpleWrite在数据量不大的情况下可以使用（5000以内，具体也要看实际情况），数据量大参照 重复多次写入
+
+        // 写法1 JDK8+
+        // since: 3.0.0-beta1
+        String fileName = "/home"+ "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+//        EasyExcel.write(fileName, DemoData.class)
+//                .sheet("模板")
+//                .doWrite(
+//                        () -> {
+//                    // 分页查询数据
+//                    return data();
+//                });
+
+        // 写法2
+        fileName = "/home"+ "simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写，然后写到第一个sheet，名字为模板 然后文件流会自动关闭
+        // 如果这里想使用03 则 传入excelType参数即可
+        EasyExcel.write(fileName, DemoData.class).sheet("模板").doWrite(data());
+
+        // 写法3
+        fileName = "/home"+ "/simpleWrite" + System.currentTimeMillis() + ".xlsx";
+        // 这里 需要指定写用哪个class去写
+        ExcelWriter excelWriter = null;
+        try {
+            excelWriter = EasyExcel.write(fileName, DemoData.class).build();
+            WriteSheet writeSheet = EasyExcel.writerSheet("模板").build();
+            excelWriter.write(data(), writeSheet);
+        } finally {
+            // 千万别忘记finish 会帮忙关闭流
+            if (excelWriter != null) {
+                excelWriter.finish();
+            }
+        }
+    }
+
+
+    private List<DemoData> data() {
+        List<DemoData> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            DemoData data = new DemoData();
+            data.setString("字符串" + i);
+            data.setDate(new Date());
+            data.setDoubleData(0.56);
+            list.add(data);
+        }
+        return list;
+    }
+
+    @Data
+    @EqualsAndHashCode
+    class DemoData {
+        @ExcelProperty("字符串标题")
+        private String string;
+        @ExcelProperty("日期标题")
+        private Date date;
+        @ExcelProperty("数字标题")
+        private Double doubleData;
+        /**
+         * 忽略这个字段
+         */
+        @ExcelIgnore
+        private String ignore;
+    }
+}
+
