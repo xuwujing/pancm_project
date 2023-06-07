223ed2fa48bf8e7c44870e945aa0f27b73c061b1
pancm <1060589146@qq.com>
2023-06-07 14:49:55 +0800

    update

--- src/main/java/com/pancm/test/jdk8/StreamTest.java
+++ src/main/java/com/pancm/test/jdk8/StreamTest.java
@@ -19,7 +19,13 @@ import org.springframework.beans.BeanUtils;
  *
  * @author pancm
  * @Title: StreamTest
- * @Description: Stream测试用例 流的操作类型分为两种：               Intermediate：一个流可以后面跟随零个或多个 intermediate               操作。其目的主要是打开流，做出某种程度的数据映射/过滤，然后返回一个新的流，交给下一个操作使用。               这类操作都是惰性化的（lazy），就是说，仅仅调用到这类方法，并没有真正开始流的遍历。 Terminal：一个流只能有一个               terminal 操作，当这个操作执行后，流就被使用“光”了，无法再被操作。 所以这必定是流的最后一个操作。 Terminal               操作的执行，才会真正开始流的遍历，并且会生成一个结果，或者一个 side effect。
+ * @Description: Stream测试用例 流的操作类型分为两种：
+ * Intermediate：一个流可以后面跟随零个或多个 intermediate
+ * 操作。其目的主要是打开流，做出某种程度的数据映射/过滤，然后返回一个新的流，交给下一个操作使用。
+ * 这类操作都是惰性化的（lazy），就是说，仅仅调用到这类方法，并没有真正开始流的遍历。
+ * Terminal：一个流只能有一个
+ * terminal 操作，当这个操作执行后，流就被使用“光”了，无法再被操作。 所以这必定是流的最后一个操作。
+ * Terminal               操作的执行，才会真正开始流的遍历，并且会生成一个结果，或者一个 side effect。
  * @Version:1.0.0
  * @date 2018年9月3日
  */
