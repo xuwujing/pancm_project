08d0f9fe553b2c945e8c70353edb5be316b6f999
pancm <1060589146@qq.com>
2023-06-09 17:13:54 +0800

    add httpsUtil

---
+++ src/main/java/com/pancm/test/httpTest/HttpsUtils.java
@@ -0,0 +1,36 @@
+package com.pancm.test.httpTest;
+
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @Author pancm
+ * @Description https请求调用
+ * @Date  2023/6/9
+ * @Param
+ * @return
+ **/
+public class HttpsUtils {
+
+    public static String sendGetRequest(String urlStr) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(urlStr);
+
+        try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String response = HttpsUtils.sendGetRequest("https://jsonplaceholder.typicode.com/posts/1");
+        System.out.println(response);
+    }
+
+}
