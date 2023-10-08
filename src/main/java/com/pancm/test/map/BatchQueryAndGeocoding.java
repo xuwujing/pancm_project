e367d798eff75fdde34d582d119577284abcaabd
pancm <1060589146@qq.com>
2023-10-08 16:00:37 +0800

    remove redundant references

--- src/main/java/com/pancm/test/map/BatchQueryAndGeocoding.java
+++ src/main/java/com/pancm/test/map/BatchQueryAndGeocoding.java
@@ -48,7 +48,7 @@ public class BatchQueryAndGeocoding {
             stmt = conn.createStatement();
 
             // 查询gaode_address表的address字段
-            String sql = "SELECT  street_code,city_name,formatted_address FROM jz_all_street where  id >6588";
+            String sql = "SELECT  street_code,city_name,formatted_address FROM jz_all_street where  center is null";
             rs = stmt.executeQuery(sql);
 
             // 遍历查询结果
@@ -81,42 +81,43 @@ public class BatchQueryAndGeocoding {
                 String streetName = jsonObject.getString("street");
                 //获取经纬度
                 String center = jsonObject.getString("location");
-                //调用逆地址编码
-                String apiUrl2 = "https://restapi.amap.com/v3/geocode/regeo";
-                // 构建请求URL
-                String requestUrl2 = apiUrl2 + "?location=" + center + "&key=" + apiKey;
-                String data2 = geocoding(requestUrl2);
-                log.info("data2" + data);
-                JSONObject geocodingResult2 = JSON.parseObject(data2);
-                JSONObject jsonObject2 = geocodingResult2.getJSONObject("regeocode");
-                JSONObject jsonObject1 = jsonObject2.getJSONObject("addressComponent");
-                String formattedAddress = jsonObject2.getString("formatted_address");
-                String cityCode = jsonObject1.getString("citycode");
-                String districtCode = jsonObject1.getString("adcode");
-                String districtName = jsonObject1.getString("district");
-                String streetCode = jsonObject1.getString("towncode");
-                streetName = jsonObject1.getString("township");
-                String provinceName = jsonObject1.getString("province");
                 String tableName = "jz_all_street";
                 String updateSql = " update " + tableName + " set center = '" + center + "' where street_code = " + streetCode2;
                 executeUpdate(conn, updateSql);
-                String insertSql = "INSERT INTO gaode_address (create_time, modified_time, creator_id, modifier_id, deleted, province_name, city_code, city_name, district_code, district_name, street_code, street_name, formatted_address, center) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-                PreparedStatement statement = conn.prepareStatement(insertSql);
-                statement.setString(1, MyTools.getNowTime());
-                statement.setString(2, MyTools.getNowTime());
-                statement.setString(3, "admin");
-                statement.setString(4, "admin");
-                statement.setInt(5, 0);
-                statement.setString(6, provinceName);
-                statement.setString(7, cityCode);
-                statement.setString(8, cityName);
-                statement.setString(9, districtCode);
-                statement.setString(10, districtName);
-                statement.setString(11, streetCode);
-                statement.setString(12, streetName);
-                statement.setString(13, formattedAddress);
-                statement.setString(14, center);
-                statement.executeUpdate();
+//                //调用逆地址编码
+//                String apiUrl2 = "https://restapi.amap.com/v3/geocode/regeo";
+//                // 构建请求URL
+//                String requestUrl2 = apiUrl2 + "?location=" + center + "&key=" + apiKey;
+//                String data2 = geocoding(requestUrl2);
+//                log.info("data2" + data);
+//                JSONObject geocodingResult2 = JSON.parseObject(data2);
+//                JSONObject jsonObject2 = geocodingResult2.getJSONObject("regeocode");
+//                JSONObject jsonObject1 = jsonObject2.getJSONObject("addressComponent");
+//                String formattedAddress = jsonObject2.getString("formatted_address");
+//                String cityCode = jsonObject1.getString("citycode");
+//                String districtCode = jsonObject1.getString("adcode");
+//                String districtName = jsonObject1.getString("district");
+//                String streetCode = jsonObject1.getString("towncode");
+//                String provinceName = jsonObject1.getString("towncode");
+//                streetName = jsonObject1.getString("township");
+//
+//                String insertSql = "INSERT INTO gaode_address (create_time, modified_time, creator_id, modifier_id, deleted, province_name, city_code, city_name, district_code, district_name, street_code, street_name, formatted_address, center) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+//                PreparedStatement statement = conn.prepareStatement(insertSql);
+//                statement.setString(1, MyTools.getNowTime());
+//                statement.setString(2, MyTools.getNowTime());
+//                statement.setString(3, "admin");
+//                statement.setString(4, "admin");
+//                statement.setInt(5, 0);
+//                statement.setString(6, provinceName);
+//                statement.setString(7, cityCode);
+//                statement.setString(8, cityName);
+//                statement.setString(9, districtCode);
+//                statement.setString(10, districtName);
+//                statement.setString(11, streetCode);
+//                statement.setString(12, streetName);
+//                statement.setString(13, formattedAddress);
+//                statement.setString(14, center);
+//                statement.executeUpdate();
                 log.info("写入成功!" + streetName);
             }
         } catch (SQLException | InterruptedException e) {
