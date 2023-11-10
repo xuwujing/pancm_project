6df8e218b9a30647fa26af7362572195b7419d8a
pancm <1060589146@qq.com>
2023-11-10 14:42:43 +0800

    update

--- src/main/java/com/pancm/util/AESEncryptDecrypt.java
+++ src/main/java/com/pancm/util/AESEncryptDecrypt.java
@@ -12,6 +12,8 @@ import javax.crypto.spec.SecretKeySpec;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+import static com.pancm.test.encrypt.BaseCodingHandler.getBytes;
+
 
 /**
  * The type Aes encrypt decrypt.
@@ -174,15 +176,9 @@ public class AESEncryptDecrypt {
 	}
 
 	private static byte[] parseHexStr2Byte(String hexStr) {
-		if (hexStr.length() < 1)
+		if (hexStr.isEmpty())
 			return null;
-		byte[] result = new byte[hexStr.length() / 2];
-		for (int i = 0; i < hexStr.length() / 2; i++) {
-			int high = Integer.parseInt(hexStr.substring(i * 2, i * 2 + 1), 16);
-			int low = Integer.parseInt(hexStr.substring(i * 2 + 1, i * 2 + 2), 16);
-			result[i] = (byte) (high * 16 + low);
-		}
-		return result;
+		return getBytes(hexStr);
 	}
 
 
