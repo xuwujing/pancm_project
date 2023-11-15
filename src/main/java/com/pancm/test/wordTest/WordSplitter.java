cdedc6b887ea21cf18ec47556539120f6b18adb2
pancm <1060589146@qq.com>
2023-11-15 16:45:50 +0800

    add word

---
+++ src/main/java/com/pancm/test/wordTest/WordSplitter.java
@@ -0,0 +1,41 @@
+package com.pancm.test.wordTest;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author pancm
+ * @Description word文件切割
+ * @Date 2023/11/15
+ * @Param
+ * @return
+ **/
+public class WordSplitter {
+    public static void main(String[] args) throws IOException {
+        File file = new File("input.docx");
+        List<String> lines = splitWordFile(file);
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+
+    public static List<String> splitWordFile(File file) throws IOException {
+        List<String> lines = new ArrayList<>();
+        try (FileInputStream fis = new FileInputStream(file)) {
+            StringBuilder content = new StringBuilder();
+            byte[] buffer = new byte[1024];
+            int len;
+            while ((len = fis.read(buffer)) != -1) {
+                content.append(new String(buffer, 0, len));
+            }
+            String[] words = content.toString().split("\\s+");
+            for (String word : words) {
+                lines.add(word);
+            }
+        }
+        return lines;
+    }
+}
---
+++ src/main/java/com/pancm/test/wordTest/WordToMarkdownConverter.java
@@ -0,0 +1,41 @@
+package com.pancm.test.wordTest;
+
+import org.apache.poi.xwpf.extractor.XWPFWordExtractor;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFPictureData;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WordToMarkdownConverter {
+
+    public static void main(String[] args) {
+        try {
+            File wordFile = new File("input.docx");
+            XWPFDocument document = new XWPFDocument(new FileInputStream(wordFile));
+            XWPFWordExtractor extractor = new XWPFWordExtractor(document);
+
+            StringBuilder markdownContent = new StringBuilder();
+
+            for (XWPFParagraph paragraph : document.getParagraphs()) {
+                markdownContent.append(paragraph.getText()).append("\n");
+            }
+
+            for (XWPFPictureData picture : document.getAllPictures()) {
+                byte[] pictureData = picture.getData();
+                // Save pictureData to file and include link in markdownContent
+            }
+
+            FileWriter writer = new FileWriter("output.md");
+            writer.write(markdownContent.toString());
+            writer.close();
+
+            System.out.println("Word document converted to Markdown successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
