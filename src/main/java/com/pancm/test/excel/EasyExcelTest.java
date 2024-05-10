4e62f2532827ed59df485166f6c22ade3f18e1a2
xuwujing <1060589146@qq.com>
2024-05-10 14:14:06 +0800

    增加工具类测试

--- src/main/java/com/pancm/test/excel/EasyExcelTest.java
+++ src/main/java/com/pancm/test/excel/EasyExcelTest.java
@@ -135,13 +135,11 @@ public class EasyExcelTest {
                 log.info("存储数据库成功！");
             }
         }).sheet().doRead();
-
         // 有个很重要的点 DemoDataListener 不能被spring管理，要每次读取excel都要new,然后里面用到spring可以构造方法传进去
         // 写法3：
         fileName = "/home" + "demo" + File.separator + "demo.xlsx";
         // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
         EasyExcel.read(fileName, DemoData.class, new DemoDataListener()).sheet().doRead();
-
         // 写法4：
         fileName = "test2.xlsx";
         // 一个文件一个reader
